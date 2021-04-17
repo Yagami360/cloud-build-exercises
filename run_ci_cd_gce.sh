cb1f3009bdfd2d6c6897d0d3df44ea83cde4f641
Yagami360 <y034112@gmail.com>
2021-04-18 00:24:08 +0900

    a

--- run_ci_cd_gce.sh
+++ run_ci_cd_gce.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -eu
+TRIGER_BRANCH_NAME=local
 
+# CI/CD トリガー発行
+git checkout -b ${TRIGER_BRANCH_NAME}
 git add .
 git commit -m "a"
-git push origin master
+git push origin ${TRIGER_BRANCH_NAME}
+
+# ビルド結果の表示
+#gcloud builds describe ${BUILD_ID}
---
+++ run_ci_cd_local.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -eu
+TRIGER_BRANCH_NAME=local
+PROJECT_ID=my-project2-303004
+IMAGE_NAME=api-sample-image
+
+# CI/CD トリガー発行
+git checkout -b ${TRIGER_BRANCH_NAME}
+git add .
+git commit -m "a"
+git push origin ${TRIGER_BRANCH_NAME}
+
+# Container Registry とやり取りするときに Container Registry 認証情報を使用するよう Docker を構成
+gcloud auth configure-docker
+
+# docker image を実行
+docker run gcr.io/${PROJECT_ID}/IMAGE_NAME
+
+# ビルド結果の表示
+#gcloud builds describe ${BUILD_ID}

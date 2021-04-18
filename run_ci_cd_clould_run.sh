3efe5d4937a5a4b514d36452d092334b9dc626fd
Yagami360 <y034112@gmail.com>
2021-04-18 12:15:22 +0900

    a

--- run_ci_cd_clould_run.sh
+++ run_ci_cd_clould_run.sh
@@ -13,10 +13,32 @@ git commit -m "a"
 git push origin ${TRIGER_BRANCH_NAME}
 
 # ビルド結果の表示
-sleep 180
-#gcloud builds describe ${BUILD_ID}
+BUILD_ID=`gcloud builds list | sed -n 2p | awk '{print $1}'`
 
-# テスト実行（リクエスト処理）
-sleep 5
-python api/request.py --host ${HOST_ADRESS} --port ${PORT} --request_value 1 --debug
-python api/request.py --host ${HOST_ADRESS} --port ${PORT} --request_value 0 --debug
+for in `seq 1000`
+do
+    echo "ビルド実行中..."
+    sleep 1
+    BUILD_STATUS=`gcloud builds list | sed -n 2p | awk '{print $6}'`
+
+    if [ ${BUILD_STATUS} = "SUCCESS" ] ; then
+        echo "ビルド成功"
+        gcloud builds describe ${BUILD_ID}
+
+        # テスト実行（リクエスト処理）
+        python api/request.py --host ${HOST_ADRESS} --port ${PORT} --request_value 1 --debug
+        python api/request.py --host ${HOST_ADRESS} --port ${PORT} --request_value 0 --debug
+    elif [ ${BUILD_STATUS} = "FAILURE" ] ; then
+        echo "ビルド失敗"
+        gcloud builds describe ${BUILD_ID}
+    elif [ ${BUILD_STATUS} = "CANCELLED" ] ; then
+        echo "ユーザーによるビルドのキャンセル"
+        gcloud builds describe ${BUILD_ID}
+    elif [ ${BUILD_STATUS} = "TIMEOUT" ] ; then
+        echo "ビルドのタイムアウト"
+        gcloud builds describe ${BUILD_ID}
+    elif [ ${BUILD_STATUS} = "FAILED" ] ; then
+        echo "ステップのタイムアウト"
+        gcloud builds describe ${BUILD_ID}
+    fi
+done
